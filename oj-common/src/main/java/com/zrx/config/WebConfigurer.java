fb078f98724541170f55f6de90c76c9c6d5b6b79
yc <3504206515@qq.com>
2025-06-17 11:47:06 +0800

    项目完善

--- oj-common/src/main/java/com/zrx/config/WebConfigurer.java
+++ oj-common/src/main/java/com/zrx/config/WebConfigurer.java
@@ -1,6 +1,7 @@
 package com.zrx.config;
 
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -16,7 +17,6 @@ import java.io.File;
  */
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
-
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// 注册 Sa-Token 拦截器，打开注解式鉴权功能
@@ -28,8 +28,10 @@ public class WebConfigurer implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		String property = System.getProperty("user.dir");
+		String uploadDir = property.replace("\\","/") + "/oj-sys/src/main/resources/user-avatars/";
 		registry.addResourceHandler("/user-avatars/**")
-				.addResourceLocations("classpath:/user-avatars/");
+				.addResourceLocations("file:" + uploadDir);
 	}
 
 }
